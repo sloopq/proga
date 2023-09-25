016be92dbce8007ab0f11a4ae36ae03a21a05f7c
sloopq <vovanchik058905@gmail.com>
2023-09-25 09:38:08 +0200

    add

--- proga_2.1_2.py
+++ proga_2.1_2.py
@@ -1,14 +1,6 @@
 n = int(input("Введите натуральное число n: "))
 
 for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(j, end='')
-    print()
-
-
-n = int(input("Введите натуральное число n: "))
-
-for i in range(1, n + 1):
     # Создаем строку, состоящую из чисел от 1 до i
     stair = ''.join(str(j) for j in range(1, i + 1))
     print(stair)
